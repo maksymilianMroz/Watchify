b65b9c1910fcb845e3aa9119a5ab62320d0dfc8a
maksymilianMroz <mrozmaksymilian@gmail.com>
2021-12-02 00:23:48 +0100

    Sass package installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
-const App = () => {
-  return <div className="App"></div>;
-};
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,7 @@
-const Header = () => {};
+import classes from "./Header.module.scss";
+
+const Header = () => {
+  return <div className={classes.container}>hey</div>;
+};
 
 export default Header;
