c89c6a5d257ecb171f12df7290f131c38bb7849f
maksymilianMroz <mrozmaksymilian@gmail.com>
2021-12-02 00:48:25 +0100

    Structure for header added.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,22 @@
+import Hamburger from "./Hamburger/Hamburger";
+import UserAvatar from "../Header/UserAvatar/UserAvatar";
+
 import classes from "./Header.module.scss";
 
 const Header = () => {
-  return <div className={classes.container}>hey</div>;
+  return (
+    <div className={classes.container}>
+      <div>
+        <Hamburger /> <h3>Home</h3>
+      </div>
+      <h1>Watchify</h1>
+      <div>
+        <div>en</div>
+        <div>x</div>
+        <UserAvatar />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
